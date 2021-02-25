eb0c93a2806997c25815fc0616b696894019456a
declanherbertson <declanherbertson@gmail.com>
2021-02-25 13:52:53 -0800

    break out user input code to module

--- Main.hs
+++ Main.hs
@@ -3,47 +3,7 @@ module Main where
 import Graphics.Gloss
 import Render
 import Game
-import Text.Read
-import Data.Char
-
-lowerString str = [ toLower loweredString | loweredString <- str]
-
-checkValidInt :: String -> Maybe Int -> Maybe Int -> Bool
-
-checkValidInt strInt Nothing Nothing
- | (readMaybe strInt :: Maybe Int) == Nothing = False
- | otherwise = True
-
-checkValidInt strInt (Just min) Nothing
- | (readMaybe strInt :: Maybe Int) == Nothing = False
- | otherwise = (read strInt :: Int) >= min
-
-checkValidInt strInt Nothing (Just max)
- | (readMaybe strInt :: Maybe Int) == Nothing = False
- | otherwise = (read strInt :: Int) <= max
-
-checkValidInt strInt (Just min) (Just max)
- | (readMaybe strInt :: Maybe Int) == Nothing = False
- | otherwise = (read strInt :: Int) >= min && (read strInt :: Int) <= max
-
-checkValidYN :: String -> Bool
-checkValidYN str = let lstr = lowerString str in (lstr == "y" || lstr == "n" || lstr == "yes" || lstr == "no")
-
-getParamInt :: String -> Maybe Int -> Maybe Int -> IO Int
-getParamInt str mmin mmax = do 
-  putStrLn(str)
-  v <- getLine
-  if checkValidInt v mmin mmax
-    then return (read v :: Int)
-    else getParamInt str mmin mmax
-
-getParamYN :: String -> IO Bool
-getParamYN str = do 
-  putStrLn(str)
-  v <- getLine
-  if checkValidYN v
-  then let lstr = lowerString v in return ((v == "y" || v == "yes") == True)
-  else getParamYN str
+import UserInput
 
 main :: IO ()
 main = do
