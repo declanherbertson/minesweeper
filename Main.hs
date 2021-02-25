37cc1dd0d328bcc7936902a054e0a377f708f15e
declanherbertson <declanherbertson@gmail.com>
2021-02-25 13:49:44 -0800

    more robust error handling

--- Main.hs
+++ Main.hs
@@ -8,21 +8,34 @@ import Data.Char
 
 lowerString str = [ toLower loweredString | loweredString <- str]
 
-checkValidInt :: String -> Bool
-checkValidInt strInt
+checkValidInt :: String -> Maybe Int -> Maybe Int -> Bool
+
+checkValidInt strInt Nothing Nothing
  | (readMaybe strInt :: Maybe Int) == Nothing = False
  | otherwise = True
+
+checkValidInt strInt (Just min) Nothing
+ | (readMaybe strInt :: Maybe Int) == Nothing = False
+ | otherwise = (read strInt :: Int) >= min
+
+checkValidInt strInt Nothing (Just max)
+ | (readMaybe strInt :: Maybe Int) == Nothing = False
+ | otherwise = (read strInt :: Int) <= max
+
+checkValidInt strInt (Just min) (Just max)
+ | (readMaybe strInt :: Maybe Int) == Nothing = False
+ | otherwise = (read strInt :: Int) >= min && (read strInt :: Int) <= max
 
 checkValidYN :: String -> Bool
 checkValidYN str = let lstr = lowerString str in (lstr == "y" || lstr == "n" || lstr == "yes" || lstr == "no")
 
-getParamInt :: String -> IO Int
-getParamInt str = do 
+getParamInt :: String -> Maybe Int -> Maybe Int -> IO Int
+getParamInt str mmin mmax = do 
   putStrLn(str)
   v <- getLine
-  if checkValidInt v
+  if checkValidInt v mmin mmax
     then return (read v :: Int)
-    else getParamInt str
+    else getParamInt str mmin mmax
 
 getParamYN :: String -> IO Bool
 getParamYN str = do 
@@ -34,9 +47,9 @@ getParamYN str = do
 
 main :: IO ()
 main = do
-  w <- getParamInt "Please enter the width of the game (as an integer):"
-  h <- getParamInt "Please enter the height of the game (as an integer):"
-  b <- getParamInt "Please enter the number of bombs in game (as an integer):"
-  s <- getParamInt "Please enter the random seed of the game (as an integer):"
+  w <- getParamInt "Please enter the width of the game (as an integer greater than 1):" (Just 2) Nothing
+  h <- getParamInt "Please enter the height of the game (as an integer greater than 1):" (Just 2) Nothing
+  b <- getParamInt ("Please enter the number of bombs in game (as an integer" ++ " less than " ++ (show (w*h)) ++ "):") (Just 0) (Just (w*h-1))
+  s <- getParamInt "Please enter the random seed of the game (as an integer of at least 0):" (Just 0) Nothing
   c <- getParamYN "Would you like to enable cheats? (y/n)"
   play window backgroundColor 30 (initialGameState w h b s c) gameAsPicture transformGame (\_ -> id)
