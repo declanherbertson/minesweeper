52b44d5a153a779f7dfbf651a5e57298111bac9e
declanherbertson <declanherbertson@gmail.com>
2021-02-25 12:44:57 -0800

    user input validation

--- Main.hs
+++ Main.hs
@@ -3,24 +3,25 @@ module Main where
 import Graphics.Gloss
 import Render
 import Game
+import Text.Read
 
+checkValidInt :: String -> Bool
+checkValidInt strInt
+ | (readMaybe strInt :: Maybe Int) == Nothing = False
+ | otherwise = True
+
+getParamInt :: String -> IO Int
+getParamInt str = do 
+  putStrLn(str)
+  v <- getLine
+  if checkValidInt v
+    then return (read v :: Int)
+    else getParamInt str
 
 main :: IO ()
 main = do
-  -- TODO add validation
-  putStrLn("Please enter the width of the game:")
-  width <- getLine
-  let w = (read width :: Int)
-  putStrLn("Please enter the height of the game:")
-  height <- getLine
-  let h = (read height :: Int)
-  putStrLn("Please enter the number of bombs in the game:")
-  numBombs <- getLine
-  let b = (read numBombs :: Int)
-  putStrLn("Please enter random seed:")
-  randomSeed <- getLine
-  let s = (read randomSeed :: Int)
-
-  -- putStrLn $ show $ initialGameState w h b s
-
+  w <- getParamInt "Please enter the width of the game (as an integer):"
+  h <- getParamInt "Please enter the height of the game (as an integer):"
+  b <- getParamInt "Please enter the number of bombs in game (as an integer):"
+  s <- getParamInt "Please enter the random seed of the game (as an integer):"
   play window backgroundColor 30 (initialGameState w h b s) gameAsPicture transformGame (\_ -> id)
