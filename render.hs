39d04613a95c835e039fd436850a978137812c0c
declanherbertson <declanherbertson@gmail.com>
2021-02-24 17:52:20 -0800

    render basics

--- render.hs
+++ render.hs
@@ -5,7 +5,16 @@ import Graphics.Gloss.Data.Color
 import Game
 
 backgroundColor = makeColor 0 0 0 255
+foregroundColor = makeColor 255 255 255 255
 
-window = InWindow "Functional" (680, 400) (100, 100)
+window = InWindow "Minesweeper" (680, 400) (100, 100)
 
-gameAsPicture _ = Blank
+gameoverScreen = blank 
+winScreen = blank
+boardScreen board = Color white (thickCircle 88 10)
+
+gameAsPicture :: GameState -> Picture
+gameAsPicture (GameState board bombs opened status width height seed)
+  | status == Start || status == Continue = boardScreen board
+  | status == Gameover = gameoverScreen
+  | otherwise = winScreen
